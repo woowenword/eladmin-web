fc6b17d932f0ad6b0aabcb8e97700f6a1acc6ea7
wuwenwen <474000418@qq.com>
2022-05-23 21:57:47 +0800

    init project

---
+++ src/views/tools/storage/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-tabs v-model="activeName" style="padding-left: 8px;" @tab-click="tabClick">
+    <el-tab-pane label="本地存储" name="first">
+      <Local ref="local" />
+    </el-tab-pane>
+  </el-tabs>
+</template>
+
+<script>
+import Local from './local/index'
+export default {
+  name: 'Storage',
+  components: { Local },
+  data() {
+    return {
+      activeName: 'first'
+    }
+  },
+  methods: {
+    tabClick(name) {
+      if (this.activeName === 'first') {
+        this.$refs.local.crud.toQuery()
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
